255013756177ef7dd33ba1af89afff1dd1fecabe
boskoa <bosko.ap@gmail.com>
2022-05-03 22:00:35 +0200

    FFS

--- phonebook-frontend/src/services/phonebook.js
+++ phonebook-frontend/src/services/phonebook.js
@@ -19,4 +19,5 @@ const updatePhonebook = (updatedEntry, id) => {
   return request.then(response => response.data)
 }
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { createEntry, getAll, removeEntry, updatePhonebook }
